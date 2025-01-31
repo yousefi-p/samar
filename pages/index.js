9c43cd988fd0f2f9c3e044062f871e11014f5cbf
POUYA <pouya.yusefi@gmail.com>
2025-01-31 22:50:14 +0330

    second

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,3 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 import { useState, useEffect } from "react";
 
